8eb11238e385503c810ed7e6bae0b39a6c29b450
kevintarjono <kevin.tarjono@gmail.com>
2020-03-20 21:25:19 +0800

    Implement hashed password and login verification

--- routes/profile.js
+++ routes/profile.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 let Profile = require("../models/profile.model");
 let User = require("../models/user.model");
 
@@ -10,54 +12,69 @@ router.route("/").get((req, res) => {
 });
 
 //get one user
-router.route("/:email").get((req, res) => {
-  Profile.find({ email: req.params.email })
-    .then(profile => {
-      //password checking
-      // if(password is correct) {
-      //     res.json(profile)
-      // }else{
-      //     res.json(error)
-      // }
-    })
-    .catch(err => res.status(400).json("Error: " + err));
-});
+router.route("/login").post((req, res) => {
+  Profile.find({email: req.body.email}).then((profile)=>{
+    var currentProfile = profile[0]
+    if(currentProfile == null){
+      return res.status(404).send("Cannot find user");
+    }
+    try{
+      bcrypt.compare(req.body.password, currentProfile.password).then(function(result) {
+        if(result == true){
+          User.find({profile: currentProfile}).then((user)=>{
+            res.send(user);
+          })
+        }
+        else{
+          res.status(401).send("Wrong password");
+        }
+      });
+    }
+    catch{
+      res.status(500).send();
+    }
+  });
+})
 
 //add new profile
 router.route("/add").post((req, res) => {
   const name = req.body.name;
-  const phoneNumber = req.body.phone;
+  const phoneNumber = req.body.phoneNumber;
   const email = req.body.email;
   const country = req.body.country;
-  const password = req.body.password; // hashed later (?)
+  const cleanPassword = req.body.password;
+  bcrypt.hash(cleanPassword, saltRounds).then((hashedPassword)=>{
+    password = hashedPassword;
 
-  Profile.find({ name }, result => {
-    if (result === null) {
-      const newProfile = new Profile({
-        name,
-        phoneNumber,
-        email,
-        country,
-        password
-      });
-      newProfile
-        .save()
-        .then(newProfile => {
-          res.json("New Profile added !");
-          console.log(newProfile);
-          var newuser = new User({ profile: newProfile });
-          newuser
-            .save()
-            .then(() => {
-              console.log("user saved");
-            })
-            .catch(err => res.status(400).json("Error: " + err));
-        })
-        .catch(err => res.status(400).json("Error: " + err));
-    } else {
-      res.json("Profile exists!");
-    }
+    Profile.find({ name }, result => {
+      if (result === null) {
+        const newProfile = new Profile({
+          name,
+          phoneNumber,
+          email,
+          country,
+          password
+        });
+        newProfile
+          .save()
+          .then(newProfile => {
+            res.json("New Profile added !");
+            console.log(newProfile);
+            var newuser = new User({ profile: newProfile });
+            newuser
+              .save()
+              .then(() => {
+                console.log("user saved");
+              })
+              .catch(err => res.status(400).json("Error: " + err));
+          })
+          .catch(err => res.status(400).json("Error: " + err));
+      } else {
+        res.json("Profile exists!");
+      }
   });
+  });
+  
 });
 
 module.exports = router;
