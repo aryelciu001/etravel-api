6bef765d6eb0a81969e4a040985243676df44566
aryelciu001 <aryelciu001@gmail.com>
2020-03-26 19:27:55 +0800

    test

--- routes/profile.js
+++ routes/profile.js
@@ -1,20 +1,18 @@
 const router = require("express").Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const saltRounds = 10;
-const verify = require('./verifyToken');
+const verify = require("./verifyToken");
 let Profile = require("../models/profile.model");
 let User = require("../models/user.model");
 
-
 //after login, user is authenticated, get current user by req.currentUser
-router.get('/afterLogin', verify, (req,res) => {
-  const currentUser = req.currentUser;          // get currentUser authenticated
-
-  User.find({_id: currentUser._id}).then((user)=>{
+router.get("/afterLogin", verify, (req, res) => {
+  const currentUser = req.currentUser; // get currentUser authenticated
+  User.find({ _id: currentUser._id }).then(user => {
     res.send(user);
-  })
-})
+  });
+});
 
 //get all users
 router.route("/").get((req, res) => {
@@ -25,32 +23,35 @@ router.route("/").get((req, res) => {
 
 //get one user
 router.route("/login").post((req, res) => {
-  Profile.find({email: req.body.email}).then((profile)=>{
-    var currentProfile = profile[0]
-    if(currentProfile == null){
+  Profile.find({ email: req.body.email }).then(profile => {
+    var currentProfile = profile[0];
+    if (currentProfile == null) {
       return res.status(404).send("Cannot find user");
     }
-    try{
-      bcrypt.compare(req.body.password, currentProfile.password).then(function(result) {
-        if(result == true){
-          User.find({profile: currentProfile}).then((user)=>{
-            const token = jwt.sign({ _id: user[0]._id }, process.env.TOKEN_SECRET);
-            res.header('auth-token', token);
-            // front end can access this to check whether user logged in or not
+    try {
+      bcrypt
+        .compare(req.body.password, currentProfile.password)
+        .then(function(result) {
+          if (result == true) {
+            User.find({ profile: currentProfile }).then(user => {
+              const token = jwt.sign(
+                { _id: user[0]._id },
+                process.env.TOKEN_SECRET
+              );
+              res.header("auth-token", token);
+              // front end can access this to check whether user logged in or not
 
-            res.send(user);
-          })
-        }
-        else{
-          res.status(401).send("Wrong password");
-        }
-      });
-    }
-    catch{
+              res.send(token);
+            });
+          } else {
+            res.status(401).send("Wrong password");
+          }
+        });
+    } catch {
       res.status(500).send();
     }
   });
-})
+});
 
 //add new profile
 router.route("/add").post((req, res) => {
@@ -59,7 +60,7 @@ router.route("/add").post((req, res) => {
   const email = req.body.email;
   const country = req.body.country;
   const cleanPassword = req.body.password;
-  bcrypt.hash(cleanPassword, saltRounds).then((hashedPassword)=>{
+  bcrypt.hash(cleanPassword, saltRounds).then(hashedPassword => {
     password = hashedPassword;
 
     Profile.find({ name }, result => {
@@ -88,9 +89,8 @@ router.route("/add").post((req, res) => {
       } else {
         res.json("Profile exists!");
       }
+    });
   });
-  });
-  
 });
 
 module.exports = router;
