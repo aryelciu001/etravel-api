d868a1f1aaab490c1290371c0711666e6897404d
aryelciu001 <aryelciu001@gmail.com>
2020-03-31 17:20:12 +0800

    Added itinerary route

--- routes/itineraryQuery.js
+++ routes/itineraryQuery.js
@@ -3,7 +3,20 @@ const axios = require("axios");
 const itineraryJSON = require("../computation/data.json");
 
 router.route("/").post((req, res) => {
-  res.send(itineraryJSON);
+  const city = req.body.city;
+  const preference = req.body.preference;
+  var response = itineraryJSON[city][preference];
+  var indices = [];
+  var toBeSent = [];
+  var index;
+  while (toBeSent.length < 10) {
+    index = Math.floor(Math.random() * response.length - 1);
+    if (!indices.includes(index)) {
+      toBeSent.push(response[index]);
+      indices.push(index);
+    }
+  }
+  res.send(toBeSent);
 });
 
 module.exports = router;
